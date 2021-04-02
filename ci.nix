81ed7175084539bccf1bb50aaf47109fee98ab82
Rodrigo Gryzinski <rogryza@gmail.com>
2021-04-02 18:37:19 -0300

    Forget nix -.-

--- ci.nix
+++
@@ -1,6 +0,0 @@
-{ channel ? "1.50.0"
-, nixpkgs ? import <nixpkgs>
-}:
-(import ./nix/shell-common.nix) {
-  inherit channel nixpkgs;
-}
--- shell.nix
+++
@@ -1,7 +0,0 @@
-{ channel ? "1.50.0"
-, nixpkgs ? import <nixpkgs>
-}:
-(import ./nix/shell-common.nix) {
-  inherit channel nixpkgs;
-  extra-packages = pkgs: [ pkgs.cargo-edit ];
-}
